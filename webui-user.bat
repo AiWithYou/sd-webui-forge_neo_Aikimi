b48c49d00234c951f327083a5b511212f450f137
Your Name <you@example.com>
2026-07-08 00:12:58 +0900

    Restore Forge model dropdowns

--- webui-user.bat
+++ webui-user.bat
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 :: set GIT=
 set "VENV_DIR=%~dp0venv"
 
-set "COMMANDLINE_ARGS=--uv --bnb --api --port 7861 --theme dark --tiled-conv2d 128"
+set "COMMANDLINE_ARGS=--uv --bnb --api --port 7861 --theme dark --tiled-conv2d 128 --forge-ref-comfy-yaml forge_neo_model_paths.yaml"
 
 :: --xformers --sage --uv
 :: --pin-shared-memory --cuda-malloc --cuda-stream
