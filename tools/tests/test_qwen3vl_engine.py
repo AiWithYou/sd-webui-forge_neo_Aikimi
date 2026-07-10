9c0ecf11cee7f7e14d1fcde90130ef842ef5cab9
AiWithYou <aiwithyou1@gmail.com>
2026-07-10 13:10:37 +0900

    Expand Krea2 prompt regression coverage

--- tools/tests/test_qwen3vl_engine.py
+++ tools/tests/test_qwen3vl_engine.py
@@ -1,5 +1,5 @@
-import unittest
 import sys
+import unittest
 
 sys.argv = sys.argv[:1]
 
@@ -25,20 +25,42 @@ class Qwen3VLTokenizeTests(unittest.TestCase):
         engine.vision_block = "<|vision_start|><|image_pad|><|vision_end|>"
         return engine, tokenizer
 
-    def test_empty_text_uses_text_template(self):
+    def test_empty_text_uses_nonempty_text_template(self):
         engine, tokenizer = self.make_engine()
 
         tokenized = engine.tokenize([""])
 
         self.assertGreater(len(tokenized[0]), 0)
-        self.assertEqual(len(tokenizer.calls), 1)
-        self.assertEqual("text:", tokenizer.calls[0][0])
+        self.assertEqual(["text: "], tokenizer.calls[0])
 
-    def test_empty_text_with_images_uses_image_template(self):
+    def test_empty_text_with_images_uses_nonempty_image_template(self):
         engine, tokenizer = self.make_engine()
 
         tokenized = engine.tokenize([""], images=[object()])
 
         self.assertGreater(len(tokenized[0]), 0)
-        self.assertEqual(len(tokenizer.calls), 1)
-        self.assertEqual("image:<|vision_start|><|image_pad|><|vision_end|>", tokenizer.calls[0][0])
+        self.assertEqual(
+            ["image:<|vision_start|><|image_pad|><|vision_end|> "],
+            tokenizer.calls[0],
+        )
+
+    def test_image_arguments_do_not_leak_between_calls(self):
+        engine, tokenizer = self.make_engine()
+
+        engine.tokenize(["first"], images=[object(), object()])
+        engine.tokenize(["second"])
+
+        self.assertEqual(
+            "image:<|vision_start|><|image_pad|><|vision_end|>"
+            "<|vision_start|><|image_pad|><|vision_end|>first",
+            tokenizer.calls[0][0],
+        )
+        self.assertEqual("text:second", tokenizer.calls[1][0])
+
+    def test_image_parameters_use_none_defaults(self):
+        self.assertEqual((None,), Qwen3VLTextProcessingEngine.tokenize.__defaults__)
+        self.assertEqual((None,), Qwen3VLTextProcessingEngine.tokenize_line.__defaults__)
+
+
+if __name__ == "__main__":
+    unittest.main()
